9867fd752b0b1a0a74a65f2ecbee78b0f71f5802
student <student@pc-108-21>
2018-03-14 17:36:48 +0100

    Lab3

--- POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
@@ -5,6 +5,8 @@
  */
 package pl.edu.ur.POLab3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asia
@@ -18,6 +20,51 @@ public class Main {
         // TODO 1.4, 1.7, 
         // TODO 2.2
         // TODO 3.3, 3.4
+        
+// String s1 = new String( "Happy " );
+//      String s2 = new String( "Birthday" );
+//
+//      System.out.printf( "s1 = %s\ns2 = %s\n\n",s1, s2 );
+//      System.out.printf( 
+//         "Result of s1.concat( s2 ) = %s\n", s1.concat( s2 ) );
+//      System.out.printf( "s1 after concatenation = %s\n", s1 );
+        
+Scanner sc = new Scanner(System.in);
+//System.out.print("Podaj liczbę: " ); 
+//// pobieramy od użytkownika liczbę
+//int liczba = sc.nextInt(); 
+//// wyświetlamy na ekranie obliczoną silnię
+//System.out.println(liczba + "! = " + silnia(liczba));
+
+
+            int n;
+            System.out.println("Podaj jaka silnie chcesz obliczyc: ");
+            n = sc.nextInt();
+           
+
+            System.out.println(n+"! = "+ silniaiter(n));
+
+
+    }
+    
+  
+    public static int silnia(int wartosc) {
+// jeśli przekazany parametr jest równy zero to zwróć 1
+// a w przeciwnym wypadku zwróć wartość parametru * wywołanie metody silnia
+// z parametrem o jeden mniejszym
+if(wartosc == 0) return 1;
+else return wartosc * silnia(wartosc - 1);
+}
+    
+    public static int silniaiter(int n)
+    {
+        int silnia = 1;
+            for(int i=1; i<=n; i++)
+            {
+                silnia*=i;
+            }
+            
+            return silnia;
     }
     
 }
